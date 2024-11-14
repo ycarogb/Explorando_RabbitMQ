6f5460dbdcf1efcc6a16407be65ab4c6c14aaca0
Ycaro Batalha <ycarobatalha1@gmail.com>
2024-11-14 12:37:29 -0300

    Filas de tarefas "Work Queues"

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -1,9 +1,8 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
 
-const string UriRabbitMQ = "";
+const string UriRabbitMQ = " ";
 const string QueueName = "";
-
 var factory = new ConnectionFactory() { Uri = new Uri(UriRabbitMQ) };
 using var connection = factory.CreateConnection(); //cria conexão com o servidor do RabbitMQ
 using var channel = connection.CreateModel(); // onde reside a maior parte da API para realizar as tarefas; "pequenas" conexões que compartilham uma conexão TCP"
@@ -11,14 +10,25 @@ using var channel = connection.CreateModel(); // onde reside a maior parte da AP
 //Declarar uma fila 
 channel.QueueDeclare(
     queue: QueueName, //nome
-    durable: true, // fila deve ser recuperada com a perda de servidor? 
+    durable: true, // fila deve ser recuperada com a perda de servidor RabbitMQ? 
     exclusive: false, // fila deve ser usada somente na mesma conexão?
     autoDelete: false, // filla deve ser deletada quando o último consumidor for cancelado/finalizado
     arguments: null);
 
 //Preparando a mensagem
-var message = "Hello World!";
+var message = GetMessage(args);
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
+
 var body = Encoding.UTF8.GetBytes(message); //conteúdo da mensagem
+
+
+//Preparando tornando mensagem persistente - RabbitMQ salva em disco
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
 
 //Publicar mensagem
 channel.BasicPublish(
--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -4,7 +4,6 @@ using RabbitMQ.Client.Events;
 
 const string UriRabbitMQ = " ";
 const string QueueName = "";
-
 var factory = new ConnectionFactory() { Uri = new Uri(UriRabbitMQ) };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
@@ -21,19 +20,29 @@ Console.WriteLine("[*] Waiting for messages.");
 var consumer = new EventingBasicConsumer(channel);
 
 //providencia um callback para aguardar a mensagem chegarem de forma assíncrona
-consumer.Received += (model, ea) =>
+//utilizando mais de um consumidor, o RabbitMQ permite paralelizar o trabalho, permitindo que as filas sejam consumidas por diferentes consumidores
+    //a fila sempre será consumida pelo próximo consumidor, em sequência, a partir da quantidade de consumidores no modelo adotado
+consumer.Received += async (model, ea) =>
 {
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
     Console.WriteLine($"[*] Received {message}");
+    
+    int dots = message.Split('.').Length -1;
+    await Task.Delay(dots * 1000);
+    
+    Console.WriteLine("[*] Done");
+    
+    //Ack = Messages ACK(nowledgments) -> mensagem de confirmação enviada pelo consumidor de que a mensagem foi processada e pode ser deletada pelo RabbitMQ
+        //Importante para garantir que nenhuma task será perdida se um consumidor "cair" por algum motivo
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 };
 
 //Consumir mensagem
 channel.BasicConsume(
     queue: QueueName, 
-    autoAck: true, 
+    autoAck: false, 
     consumer: consumer);
 
 Console.WriteLine("Press [enter] to exit.");
 Console.ReadLine();
-                    
