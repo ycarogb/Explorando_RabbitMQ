31c272383dc5590c0f26765ebe7600478ca4248f
Ycaro Batalha <ycarobatalha1@gmail.com>
2024-11-18 16:42:42 -0300

    Padrão Routing
    - Possibilita direcionar que os consumidores processem apenas determinadas mensagens que forem configuradas para tal

--- RabbitmqConfigs/RabbitmqConnection.cs
+++ RabbitmqConfigs/RabbitmqConnection.cs
@@ -2,5 +2,5 @@
 
 public static class RabbitmqConnection
 {
-    public static string UriConnection => "";
+    public static string UriConnection => "amqp://pg-develop:Softrabbit2024@172.21.42.209:5672";
 }
